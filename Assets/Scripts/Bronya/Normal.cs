f29f2f3da30f1d62898c180c3c340a56345439f3
Sebastian <36393893+SetHet@users.noreply.github.com>
2020-02-23 20:16:33 -0300

    Bronya normal dash
    Agregado el dash v1 (solo funcional), pero se tuvo que cambiar la escala del master por problemas de size de la colision

--- Assets/Scripts/Bronya/Normal.cs
+++ Assets/Scripts/Bronya/Normal.cs
@@ -9,6 +9,8 @@ namespace Bronya
         bool grounded = false;
         public float velJump = 4.5f;
         public float velMov = 3f;
+        public float distanceDash = 5f;
+        public LayerMask DashCollision = -1;
         public Master master;
 
         private void OnEnable()
@@ -58,8 +60,9 @@ namespace Bronya
             }
 
             //Cambiar direccion
-            if (Input.GetAxis("Horizontal") > 0f) transform.localScale = new Vector3(1, 1, 1); //Right
-            else if (Input.GetAxis("Horizontal") < 0f) transform.localScale = new Vector3(-1, 1, 1); //Left
+            float valueScale = 0.36f;
+            if (Input.GetAxis("Horizontal") > 0f) transform.localScale = new Vector3(1, 1, 1) * valueScale; //Right
+            else if (Input.GetAxis("Horizontal") < 0f) transform.localScale = new Vector3(-1, 1, 1) * valueScale; //Left
 
             //Saltar
             if (grounded && Input.GetKeyDown(KeyCode.Space))
@@ -68,6 +71,25 @@ namespace Bronya
                 vel += Vector2.up * velJump;
                 master.rigidbody2D.velocity = vel;
             }
+
+            //Dash
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Debug.Log("Dash");
+                BoxCollider2D coll = master.boxCollider;
+                Vector2 direction = new Vector2(transform.localScale.x, 0).normalized;
+                Vector2 addPosition;
+
+                //RaycastHit2D[] hits = Physics2D.BoxCastAll((Vector2)coll.transform.position + coll.offset, coll.size, 0, direction, distanceDash, DashCollision);
+                RaycastHit2D hit = Physics2D.BoxCast((Vector2)coll.transform.position + coll.offset, coll.size * 0.95f, 0, direction, distanceDash, DashCollision);
+                if (hit == false) addPosition = direction.normalized * distanceDash;
+                else addPosition = direction.normalized * hit.distance;
+                Debug.Log("Add position: " + addPosition);
+                //Debug.Log("Hit: " + hit.collider.name);
+
+                master.rigidbody2D.transform.position += (Vector3)addPosition;
+            }
+
         }
     }
 }
