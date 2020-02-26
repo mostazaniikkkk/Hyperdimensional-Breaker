c45e6223cb545fbce738d24d500b3c15bf64da6f
Sebastian <36393893+SetHet@users.noreply.github.com>
2020-02-26 04:48:46 -0300

    Luces de bronya dinamicas

---
+++ Assets/Scripts/Bronya/BLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bronya
+{
+    public class BLight : MonoBehaviour
+    {
+        public List<Light> myLights;
+        public List<Light> otherLights;
+
+        public void AddLight(Light light)
+        {
+            otherLights.Add(light);
+            foreach (Light item in myLights)
+            {
+                item.gameObject.SetActive(false);
+            }
+        }
+
+        public void RemoveLight(Light light)
+        {
+            otherLights.Remove(light);
+            if (otherLights.Count == 0)
+                foreach (Light item in myLights)
+                {
+                    item.gameObject.SetActive(true);
+                }
+        }
+    }
+}
---
+++ Assets/Scripts/OtherLight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherLight : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.transform.root.GetComponent<Bronya.BLight>()?.AddLight(GetComponent<Light>());
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.transform.root.GetComponent<Bronya.BLight>()?.RemoveLight(GetComponent<Light>());
+    }
+}
