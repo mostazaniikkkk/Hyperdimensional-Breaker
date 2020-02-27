e32cc335ba4afd1091c2ee37432cbb60db26fe83
Sebastian <36393893+SetHet@users.noreply.github.com>
2020-02-26 21:22:15 -0300

    Codigo Heal

---
+++ Assets/Scripts/Bronya/BHeal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bronya
+{
+    public class BHeal : MonoBehaviour
+    {
+        public LayerMask layerBronya;
+        public float heal = 25f;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (((1 << collision.gameObject.layer) & layerBronya) != 0)
+            {
+                collision.transform.root.GetComponent<Master>().stats.Heal(heal);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
