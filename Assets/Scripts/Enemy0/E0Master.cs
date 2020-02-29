75dbafb5fe8119c03827603e5126fffb784a604c
Sebastian <36393893+SetHet@users.noreply.github.com>
2020-02-29 00:42:47 -0300

    Update enemy

---
+++ Assets/Scripts/Enemy0/E0Master.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnemyMaster0
+{
+    public class E0Master : MonoBehaviour
+    {
+        bool alert = false;
+        [Min(1)] public float distanceDetection = 10;
+        [Min(1)] public float maxTimeLost = 5;
+        [Header("Components")]
+        public Rigidbody2D rigid2D;
+        public CapsuleCollider2D capCollider;
+
+        private void Update()
+        {
+            
+        }
+    }
+}
